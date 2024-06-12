5ff3b04fb75cce1ac344acf8ecd3baa4b861bbe9
matteo03s <167012344+matteo03s@users.noreply.github.com>
2024-06-12 21:02:55 +0200

    Delete src/customException directory

--- src/customException/FormatoFileNonValidoException.java
+++
@@ -1,7 +0,0 @@
-package customException;
-
-public class FormatoFileNonValidoException extends Exception {
-	public FormatoFileNonValidoException () { }
-	public FormatoFileNonValidoException (String msg) { super (msg); }
-
-}
